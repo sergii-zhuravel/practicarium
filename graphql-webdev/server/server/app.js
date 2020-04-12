5957317ac9cc78ab70ddbbb995089e8f5239468b
Serhii Zhuravel <serhii.zhuravel@temy.co>
2020-04-12 13:34:26 +0300

    graphql - connecting mongoose

--- graphql-webdev/server/server/app.js
+++ graphql-webdev/server/server/app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("../schema/schema.js");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = 3005;
 
+mongoose.connect(
+  "mongodb+srv://tutorial_user:Testing12345@cluster0-z4y9c.mongodb.net/graphql-tutorial?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
 app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
+
+const dbConnection = mongoose.connection;
+dbConnection.on("error", (err) => console.log(`Connection error: ${err}`));
+dbConnection.once("open", () => console.log("Connected to DB!"));
 
 app.listen(PORT, (err) => {
   err ? console.log(err) : console.log("Server started on PORT", PORT);
