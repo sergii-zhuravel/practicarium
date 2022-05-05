187b27ea2b129eebf8be189bf3153df55bcafa4b
Sergii Zhuravel <szhuravell@gmail.com>
2022-05-05 18:10:36 +0300

    First 7 lessons

---
+++ gatsby-tutorial/web-warrior/src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+export default function About() {
+  return (
+    <Layout>
+      <div>
+        <h1>About page</h1>
+        <p>lorem ipsum</p>
+      </div>
+    </Layout>
+  )
+}
