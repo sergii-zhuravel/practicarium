d71f1d777024883af48b6b550227389ec5e9a9ad
Sergii Zhuravel <szhuravell@gmail.com>
2022-05-06 16:13:28 +0300

    Optimizing images

--- gatsby-tutorial/web-warrior/gatsby-config.js
+++ gatsby-tutorial/web-warrior/gatsby-config.js
@@ -7,12 +7,21 @@
 module.exports = {
   /* Your site config here */
   plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     `gatsby-transformer-remark`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `projects`,
         path: `${__dirname}/src/projects/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
       },
     },
   ],
