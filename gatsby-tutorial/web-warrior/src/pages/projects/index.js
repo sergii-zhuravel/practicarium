0e94fb0c97a1b1a98ddc7e12da5b02d88f91a63f
Sergii Zhuravel <szhuravell@gmail.com>
2022-05-07 13:11:50 +0300

    Feature images

--- gatsby-tutorial/web-warrior/src/pages/projects/index.js
+++ gatsby-tutorial/web-warrior/src/pages/projects/index.js
@@ -2,6 +2,7 @@ import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../../components/Layout"
 import * as styles from "../../styles/projects.module.css"
+import Img from "gatsby-image"
 
 const Projects = ({ data }) => {
   const projects = data.projects.nodes
@@ -16,6 +17,7 @@ const Projects = ({ data }) => {
           {projects.map(project => (
             <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
               <div>
+                <Img fluid={project.frontmatter.thumb.childImageSharp.fluid} />
                 <h3>{project.frontmatter.title}</h3>
                 <p>{project.frontmatter.stack}</p>
               </div>
@@ -41,6 +43,13 @@ export const query = graphql`
           slug
           stack
           title
+          thumb {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
         }
         id
       }
